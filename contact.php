9c62416d75a99175d7012255317bcd072cfaa166
Soumit Das <its.soumit.das@gmail.com>
2023-02-13 13:53:08 +0530

    Anything

---
+++ contact.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html> 
+<head>
+		<!-- meta tag -->
+		<meta charset="utf-8">
+		<title>Contact Us | Tunnel Experts</title>
+		<?php include('stylesheet.php'); ?>
+		
+	</head>
+	<body class="defult-home">
+		
+		<div class="offwrap"></div>
+
+		<!--Preloader start here-->
+	   	<div id="pre-load">
+            <div id="loader" class="loader">
+                <div class="loader-container">
+                    <div class="loader-icon"><img src="assets/images/logo-te.png" alt="Tunnel Experts"></div>
+                </div>
+            </div>              
+        </div>
+		<!--Preloader area end here-->
+			
+		<!-- Main content Start -->
+		<div class="main-content">
+
+			<?php include('header.php'); ?>
+
+			<!-- Breadcrumbs Start -->
+			<div class="rs-breadcrumbs img1">
+			    <div class="container">
+			        <div class="breadcrumbs-inner">
+			            <h1 class="page-title">
+			               	Let's get in touch
+			            </h1>
+			            <ul class="breadcrumbs-area">
+                           <li title="Go to konstruk">
+                               <a class="active" href="index">Home</a>
+                           </li>
+                          <li>Contact Us</li>
+                       </ul>
+			        </div>
+			    </div>
+			</div>
+			<!-- Breadcrumbs End -->
+		
+			<div class="rs-contact contact-style6 pb-120 md-pb-80">
+			    <!-- Contact Icons Section End -->
+			    <div class="rs-contact pt-120 md-pt-80">
+			        <div class="container">
+			            <div class="row">
+			                <div class="col-lg-4 col-md-6 md-mb-30">
+			                   <div class="contact-box">
+			                       <div class="contact-icon">
+			                           <img src="assets/images/contact/style1/icons/location.png" alt="images">
+			                       </div>
+			                       <div class="content-text">
+			                           <h4 class="title"><a href="#">Office address</a></h4>
+			                           <p class="services-txt"> F29, A Star City Mall, Mayur <br>
+										Vihar Phase 1, Delhi - 110091 </p>
+			                       </div>
+			                   </div>
+			                </div>
+			                <div class="col-lg-4 col-md-6 sm-mb-30">
+			                   <div class="contact-box">
+			                       <div class="contact-icon">
+			                           <img src="assets/images/contact/style1/icons/email.png" alt="images">
+			                       </div>
+			                       <div class="content-text">
+			                           <h4 class="title"><a href="#">Email us</a></h4>
+			                           <span><a href="mailto:info.tunnelexperts@gmail.com">info.tunnelexperts@gmail.com</a></span><br>
+			                       </div>
+			                   </div>
+			                </div>
+			                <div class="col-lg-4 col-md-6">
+			                   <div class="contact-box">
+			                       <div class="contact-icon">
+			                           <img src="assets/images/contact/style1/icons/call.png" alt="images">
+			                       </div>
+			                       <div class="content-text">
+			                           <h4 class="title"><a href="#">Call us</a></h4>
+			                           <span><a href="tel:+919877049877">+91 98770 49877</a></span><br>
+			                       </div>
+			                   </div>
+			                </div>
+			            </div>
+			        </div>
+			    </div>
+			    <!-- Contact Icons Section End -->
+			</div>
+
+		</div> 
+		<!-- Main content End -->
+	 
+		<?php include('footer.php'); ?>
+
+		<!-- start scrollUp  -->
+		<div id="scrollUp">
+			<i class="fa fa-angle-up"></i>
+		</div>
+		<!-- End scrollUp  -->
+
+		<?php include('js.php'); ?>
+        <script>
+            var element = document.getElementById("head-contact");
+            element.classList.add("current-menu-item");
+        </script>
+	</body>
+</html>
